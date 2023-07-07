fc80af0a76d73196297cfdc6fc0fa734374de8de
vpilodausa <138843424+vpilodausa@users.noreply.github.com>
2023-07-06 22:55:47 -0500

    Add files via upload

---
+++ st_web_journal.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  6 21:52:28 2023
+
+@author: vpiloda
+"""
+
+import streamlit as st
+import altair as alt
+import re
+import pandas as pd
+import numpy as np
+import os
+os.chdir("/Users/vpiloda/Documents/LLC_Python")
+#from st_web_additional import  heatmap_plot
+
+
+import streamlit.components.v1 as components
+
+
+
+
+
+
+def my_function():
+
+
+    components.html(
+
+        """
+        <html>
+    <head>
+        <title>Yearly Symbol Prices</title>
+    </head>
+    <frameset>
+        <frame src=''>
+    </frameset>
+    </html>"""
+            
+    
+
+
+    )
+
+
+
+
+
+
+if __name__ == '__main__':
+    
+    st.write("Let's begin !")
+    
+
+    my_function()
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
