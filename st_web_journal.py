ab71ae633c7426c7fb65a88f98b67424a01fc511
vpilodausa <138843424+vpilodausa@users.noreply.github.com>
2023-07-06 23:43:14 -0500

    Update st_web_journal.py

--- st_web_journal.py
+++ st_web_journal.py
@@ -1,30 +1,5 @@
 
 
-import streamlit as st
-import altair as alt
-import re
-import pandas as pd
-import numpy as np
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-
-    x = st.text_input('Type something here')
-    
-    st.write(f"Let's begin !  Mr. {x}")
-    
-
-    
-    
-
-
 
 
 
