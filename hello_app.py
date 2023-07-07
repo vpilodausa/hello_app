dad755014fa8580781e250af875513c37cf34687
vpilodausa <138843424+vpilodausa@users.noreply.github.com>
2023-07-07 01:37:42 -0500

    Update hello_app.py

--- hello_app.py
+++ hello_app.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  6 21:52:28 2023
+
+@author: vpiloda
+"""
 
 import streamlit as st
 import altair as alt
@@ -8,31 +15,108 @@ import os
 
 
 
+import streamlit.components.v1 as components
+
+
+
+
+
+import base64
+import streamlit as st
+import plotly.express as px
+
+df = px.data.iris()
+
+
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+
+
+
+image_weblink = 'https://images.unsplash.com/photo-1673632156153-d3baf0d0401b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80'
+
+
+
+
+st.text_area(" Type something here")
 
 
 
 
 
 
-if __name__ == '__main__':
 
-    x = st.text_input('Type something here')
-    
-    st.write(f"Let's begin !  Mr. {x}")
 
-    st.write('Hola')
+img = get_img_as_base64("4.jpg")
 
-    st.write(os. getcwd())
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("{image_weblink}");
+background-size: 180%;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
 
-    
-    os.makedirs('/data')
-    os.makedirs('/vishal/data')
+[data-testid="stSidebar"] > div:first-child {{
+background-image: url("data:image/png;base64,{img}");
+background-position: center; 
+background-repeat: no-repeat;
+background-attachment: fixed;
+}}
 
-    
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
 
-    
-    
-    
-    
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+st.title("It's summer!")
+st.sidebar.header("Configuration")
+
+with st.container():
+    st.header("Big one")
+    st.markdown(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    )
+    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
+with st.container():
+    st.header("Big 2")
+    st.markdown(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    )
+    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
+with st.container():
+    st.header("Big 3")
+    st.markdown(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    )
+    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
+with st.container():
+    st.header("Big 4")
+    st.markdown(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    )
+    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
+
+
+
+
+
+
 
 
