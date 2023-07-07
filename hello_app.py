876b4caa336c84042f7e6383c072d9575784497f
vpilodausa <138843424+vpilodausa@users.noreply.github.com>
2023-07-07 01:39:21 -0500

    Update hello_app.py

--- hello_app.py
+++ hello_app.py
@@ -23,9 +23,9 @@ import streamlit.components.v1 as components
 
 import base64
 import streamlit as st
-import plotly.express as px
 
-df = px.data.iris()
+
+
 
 
 def get_img_as_base64(file):
@@ -87,33 +87,6 @@ right: 2rem;
 st.markdown(page_bg_img, unsafe_allow_html=True)
 st.title("It's summer!")
 st.sidebar.header("Configuration")
-
-with st.container():
-    st.header("Big one")
-    st.markdown(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    )
-    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
-with st.container():
-    st.header("Big 2")
-    st.markdown(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    )
-    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
-with st.container():
-    st.header("Big 3")
-    st.markdown(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    )
-    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
-with st.container():
-    st.header("Big 4")
-    st.markdown(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    )
-    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
-
-
 
 
 
