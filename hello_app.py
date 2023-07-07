5532a5cb735b27bcac7c934024ba54c8aece8fdd
vpilodausa <138843424+vpilodausa@users.noreply.github.com>
2023-07-06 23:45:13 -0500

    Update hello_app.py

--- hello_app.py
+++ hello_app.py
@@ -1,1 +1,26 @@
 
+import streamlit as st
+import altair as alt
+import re
+import pandas as pd
+import numpy as np
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    x = st.text_input('Type something here')
+    
+    st.write(f"Let's begin !  Mr. {x}")
+    
+
+    
+    
+
+
