afe9290a0306386b9d3e8e85272abf3ac266c891
vpilodausa <138843424+vpilodausa@users.noreply.github.com>
2023-07-06 23:41:28 -0500

    Update st_web_journal.py

--- st_web_journal.py
+++ st_web_journal.py
@@ -1,47 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul  6 21:52:28 2023
 
-@author: vpiloda
-"""
 
 import streamlit as st
 import altair as alt
 import re
 import pandas as pd
 import numpy as np
-import os
-os.chdir("/Users/vpiloda/Documents/LLC_Python")
-#from st_web_additional import  heatmap_plot
-
-
-import streamlit.components.v1 as components
 
 
 
-
-
-
-def my_function():
-
-
-    components.html(
-
-        """
-        <html>
-    <head>
-        <title>Yearly Symbol Prices</title>
-    </head>
-    <frameset>
-        <frame src=''>
-    </frameset>
-    </html>"""
-            
-    
-
-
-    )
 
 
 
@@ -49,12 +15,12 @@ def my_function():
 
 
 if __name__ == '__main__':
+
+    x = st.text_input('Type something here')
     
-    st.write("Let's begin !")
+    st.write(f"Let's begin !  Mr. {x}")
     
 
-    my_function()
-    
     
     
 
