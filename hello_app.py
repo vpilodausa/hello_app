396b6a1caf576137396760858ce85a27562da79c
vpilodausa <138843424+vpilodausa@users.noreply.github.com>
2023-07-07 02:05:22 -0500

    Update hello_app.py

--- hello_app.py
+++ hello_app.py
@@ -1,95 +1,49 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul  6 21:52:28 2023
 
-@author: vpiloda
-"""
 
 import streamlit as st
 import altair as alt
 import re
 import pandas as pd
 import numpy as np
-import os
-
-
-
-import streamlit.components.v1 as components
 
 
 
 
+def write_to_file(file_name, data_dict): # takes the dictionary and save it file with newlines
+    with open(file_name, "w") as f:
+        f.write(str(data_dict))
 
-import base64
-import streamlit as st
-
-
-
-
-
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
+def read_from_file(file_name): # read the file and gives dictionary
+    with open(file_name, "r") as f:
+        data_text = f.read()
+        data_dict = eval(data_text)
+        return data_dict
 
 
-
-
-
-image_weblink = 'https://images.unsplash.com/photo-1673632156153-d3baf0d0401b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80'
-
-
-
-
-st.text_area(" Type something here")
-
-
-
-
-
-
-
-
-#img = get_img_as_base64("4.jpg")
-
-page_bg_img = f"""
-<style>
-[data-testid="stAppViewContainer"] > .main {{
-background-image: url("{image_weblink}");
-background-size: 180%;
-background-position: top left;
-background-repeat: no-repeat;
-background-attachment: local;
-}}
-
-[data-testid="stSidebar"] > div:first-child {{
-background-image: url("data:image/png;base64,{img}");
-background-position: center; 
-background-repeat: no-repeat;
-background-attachment: fixed;
-}}
-
-[data-testid="stHeader"] {{
-background: rgba(0,0,0,0);
-}}
-
-[data-testid="stHeader"] {{
-background: rgba(0,0,0,0);
-}}
-
-[data-testid="stToolbar"] {{
-right: 2rem;
-}}
-</style>
-"""
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
-st.title("It's summer!")
-st.sidebar.header("Configuration")
-
-
-
-
-
-
+pass_code = 'yo'
+if st.text_input('enter the code') == pass_code:
+    
+    
+    file_name = 'data_dict.txt'
+    data_dict = read_from_file(file_name)
+    
+    
+    
+    title = st.text_input("What do you want to write about ?")
+    
+    body = st.text_area("Start .... ", height=200, max_chars=None)
+    
+    if st.button("save entry "):
+        data_dict[title] = body
+        write_to_file(file_name, data_dict)
+        
+    
+    if st.button("read previous entries "):
+        st.write(data_dict)
+    
+    
+    
+    
+    
+    
+    
